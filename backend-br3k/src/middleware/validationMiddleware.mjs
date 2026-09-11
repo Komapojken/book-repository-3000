27c3d44aedab603d65bfe492b782b678cd621d58
Robban <robert.karivainio@hotmail.se>
2026-09-11 10:18:19 +0200

    feat: Include message on validation 400 responses. Test passed.

--- backend-br3k/src/middleware/validationMiddleware.mjs
+++ backend-br3k/src/middleware/validationMiddleware.mjs
@@ -4,9 +4,7 @@ function handleValidationErrors(req, res, next) {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-        return res.status(400).json({
-            errors: errors.array()
-        });
+        return res.status(400).json({ message: "Validation failed", errors: errors.array() });
     }
 
     next();
